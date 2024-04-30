f9c87ebdae273d7c3fd04acd4d31ad3ca9607f87
Tiago Inaba <tiago.inaba@gmail.com>
2024-04-30 19:34:10 -0300

    feat: agora o output é JSON

--- __main__.py
+++ __main__.py
@@ -8,12 +8,18 @@ def get_signal_strength(interface):
     iface.scan()
     time.sleep(2)  # Wait for scanning to complete
     networks = iface.scan_results()
+    print("[")
     for network in networks:
-        print("SSID:", network.ssid, "BSSID:", network.bssid, "Signal Strength (dBm):", network.signal)
+        close = "},"
+        if network == networks[-1]:
+            close = "}"
+        print("{", "\"SSID\":", f"\"{network.ssid}\",", "\"BSSID\":", \
+              f"\"{network.bssid}\",", "\"Signal\":", f"\"{network.signal}\",", close)
+    print("]")
 
 if __name__ == "__main__":
     if platform.system() == "Windows":
         interface_index = 0  # Change this to the index of your WiFi interface
         get_signal_strength(interface_index)
-    elif platform.system() == "Linux":
+    else:
         print("Unsupported platform!")
